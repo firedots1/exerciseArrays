0121bb68059b92b81d2be6619168195c310de150
firedot1 <zcheng1@montgomerycollege.edu>
2020-04-16 19:42:05 -0400

    first commit

---
+++ src/exerciseArrays.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class exerciseArrays {
+    public static void main(String args[]) {
+        int[] num = new int[5];
+        String[] str = new String[5];
+        char ch[] = new char[5];
+        boolean bo[] = new boolean[2];
+        ch[0] = 'A';
+        ch[1] = 'B';
+        ch[2] = 'C';
+        ch[3] = 'E';
+        ch[4] = 'D';
+        num[0] = 20;
+        num[1] = 30;
+        num[2] = 40;
+        num[3] = 50;
+        num[4] = 60;
+        str[0] = "Sunday";
+        str[1] = "Monday";
+        str[2] = "Tuesday";
+        str[3] = "Wednesday";
+        str[4] = "Thursday";
+        bo[0] = true;
+        bo[1] = false;
+        System.out.print("Numbers: ");
+        System.out.println(Arrays.toString(num));
+        System.out.print("Strings: ");
+        System.out.println(Arrays.toString(str));
+        System.out.print("Characters: ");
+        System.out.println(Arrays.toString(ch));
+        System.out.print("Booleans:" );
+        System.out.println(Arrays.toString(bo));
+            }
+        }
+
+
+
+
+
+
